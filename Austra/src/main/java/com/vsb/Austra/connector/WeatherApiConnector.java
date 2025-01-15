fd83d21ad74fbf11349a886dca723bfd7cc3699a
MarinaP-prog <marina.pavolova@gmail.com>
2025-01-15 16:25:29 +0100

    Update WeatherApiConnector.java

--- Austra/src/main/java/com/vsb/Austra/connector/WeatherApiConnector.java
+++ Austra/src/main/java/com/vsb/Austra/connector/WeatherApiConnector.java
@@ -11,10 +11,11 @@ import java.net.URISyntaxException;
 public class WeatherApiConnector {
     //https://api.weatherapi.com/v1/current.json?key=2f3d5a0c8d0a4dd7986132149232003&q=stara rudna&aqi=no
     //https://api.weatherapi.com/v1/forecast.json?key=2f3d5a0c8d0a4dd7986132149232003&q=stara rudna&days=1&aqi=no&alerts=no
-    private static String baseUrl = "https://api.weatherapi.com/v1/";
+     private static String baseUrl = "https://api.weatherapi.com/v1/";
     private static String urlParams = "current.json?key=";
-    private static String apiKey = "2f3d5a0c8d0a4dd7986132149232003";
+    private static String apiKey = "799d2afe86bb453f837220513241512";
     private static String url = baseUrl+urlParams+apiKey+"&q=";
+
 
     public WeatherApiDto getWeatherForCity(City city){
         RestTemplate template = new RestTemplate();
